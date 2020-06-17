2c170a5fdedfc01c15383f571e616e84dc8b21cf
Tello- <lollisjosh@gmail.com>
2020-06-17 12:43:04 -0700

    Finished with single link node class for now

--- Linked_Node/Node.hpp
+++ Linked_Node/Node.hpp
@@ -7,9 +7,15 @@ public:
 				Node() = default;			// default ctor
 				Node(const Node<T>& rhs);	// copy ctor
 				~Node();
+
+	const T&		data() const;
+	Node<T>*		next() const;
+
+	void			setData(const T& data);
+	void			setNext(Node<T>* next);
 private:
-	T			m_data{};
-	Node<T>*		m_next{ nullptr };
+	T			m_data{};					// uses T{} default ctor for default construction of node
+	Node<T>*		m_next{ nullptr };			// default init node next to nullptr
 };
 
 template<class T>
@@ -21,4 +27,28 @@ inline Node<T>::Node(const Node<T>& rhs)
 template<class T>
 inline Node<T>::~Node()
 {
+}
+
+template<class T>
+inline const T& Node<T>::data() const
+{
+	return m_data;
+}
+
+template<class T>
+inline Node<T>* Node<T>::next() const
+{
+	return m_next;
+}
+
+template<class T>
+inline void Node<T>::setData(const T& data)
+{
+	m_data = data;
+}
+
+template<class T>
+inline void Node<T>::setNext(Node<T>* next)
+{
+	m_next = next;
 }
