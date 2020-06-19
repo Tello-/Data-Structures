974d374bbe5c89444f5d3e3d4b260833b4b8673f
Tello- <lollisjosh@gmail.com>
2020-06-18 17:22:16 -0700

    Began implementing a linked list built upon a built in array

--- Array_Linked_List/Linked_Array.hpp
+++ Array_Linked_List/Linked_Array.hpp
@@ -1,3 +1,8 @@
+/* This is a file describing a Linked List built from a built in array. The list expands as needed.
+	Note, indexed access makes no sense for this sort of list, so don't implement operator[] 
+*/
+
+
 #pragma once
 
 
@@ -5,7 +10,7 @@ template <class DT>
 struct Indexed_Node
 {
 	DT m_data{ T{} };
-	size_t m_next{ -1 }; // "pointer" to next node where the pointer is an index to another array loc.
+	size_t m_next{ -1 }; // -1 is uninitialized state
 };
 
 template<class T>
@@ -15,8 +20,39 @@ public:
 					LinkedArray() = default;
 					~LinkedArray() = default;
 
+	bool				empty()		const;
+	size_t			size()		const;
+	size_t			maxSize()		const;
+
 
 private:
-	size_t			m_headIdx{ 0 };
-	size_t			m_tailIdx{ 0 };
+	size_t			m_maxSize		{ 10 };
+	size_t			m_size		{ 0 };
+	Indexed_Node<T>*	m_pNodeArray	{ new Indexed_Node<T>[m_maxSize] };
+	size_t			m_headIdx		{ -1 };
+	size_t			m_tailIdx		{ -1 };
+	
+
+
+
+
 };
+
+template<class T>
+inline bool LinkedArray<T>::empty() const
+{
+	if (m_headIdx == -1) return true;
+	return false;
+}
+
+template<class T>
+inline size_t LinkedArray<T>::size() const
+{
+	return size_t();
+}
+
+template<class T>
+inline size_t LinkedArray<T>::maxSize() const
+{
+	return m_maxSize;
+}
